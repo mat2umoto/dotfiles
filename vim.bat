fc22efacdc20b8f899857beea5520c36fdc6c6cf
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2016-04-17 22:55:11 +0900

    bat修正

--- vim.bat
+++ vim.bat
@@ -3,5 +3,7 @@
 :: vim設定ファイルのシンボリックリンク
 mklink %HOMEPATH%\.vimrc .\.vimrc
 mklink %HOMEPATH%\.gvimrc .\.gvimrc
+mkdir %HOMEPATH%\vimfiles
+mkdir %HOMEPATH%\vimfiles\colors
 mklink %HOMEPATH%\vimfiles\colors\jellybeans.vim .\colors\jellybeans.vim
 pause
