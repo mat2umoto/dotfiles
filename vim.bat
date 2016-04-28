238b91686059454636a18f54ef6d8d828c264a29
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2016-04-29 04:32:14 +0900

    管理者として実行したときのための、カレントディレクトリ設定

--- vim.bat
+++ vim.bat
@@ -1,6 +1,9 @@
-::@echo off
+rem @echo off
 
-:: vim設定ファイルのシンボリックリンク
+rem 管理者として実行したときのための、カレントディレクトリ設定
+cd /d %~dp0
+
+rem vim設定ファイルのシンボリックリンク
 mklink %HOMEPATH%\.vimrc .\.vimrc
 mklink %HOMEPATH%\.gvimrc .\.gvimrc
 mkdir %HOMEPATH%\vimfiles
