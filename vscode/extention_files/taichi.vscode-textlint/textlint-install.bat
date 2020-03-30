f9216d397681f9777cbdf8500a1907c90453760c
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2020-03-30 17:14:38 +0900

    vscodeの設定を更新

--- vscode/extention_files/taichi.vscode-textlint/textlint-install.bat
+++ vscode/extention_files/taichi.vscode-textlint/textlint-install.bat
@@ -3,23 +3,49 @@
 rem ユーザーディレクトリへ移動
 rem cd %userprofile%
 
-rem textlint本体とルールをカレントへインストール
+rem textlint本体をカレントへインストール
+echo ****************************************************
+echo install textlint
+echo ****************************************************
 call npm i textlint
-rem *****************************************************
-rem ルール：技術文書
+
+rem ルールをインストール：技術文書
+echo ****************************************************
+echo install textlint-rule-preset-ja-technical-writing
+echo ****************************************************
 call npm i textlint-rule-preset-ja-technical-writing
-rem ルール：スペース挿入
+echo;
+
+rem ルールをインストール：スペース挿入
+echo ****************************************************
+echo install textlint-rule-preset-ja-spacing
+echo ****************************************************
 call npm i textlint-rule-preset-ja-spacing
-rem *****************************************************
+echo;
+
+rem フィルタをインストール：ホワイトリスト除外機能
+echo ****************************************************
+echo install textlint-filter-rule-whitelist
+echo ****************************************************
+call npm i textlint-filter-rule-whitelist
+echo;
 
 rem textlintのプロジェクトを初期化
+echo ****************************************************
+echo initialize textlint
+echo ****************************************************
 call .\node_modules\.bin\textlint --init
+echo;
 
 set rcfilebase=.textlintrc.base
 set rcfile=.textlintrc
 
 rem textlintのrcファイルを設定済みのものに置き換える
+echo ****************************************************
+echo make .textlintrc
+echo ****************************************************
 if exist %rcfile% del %rcfile%
 copy %rcfilebase% %rcfile%
+echo;
 
 pause
